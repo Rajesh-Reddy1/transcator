cdb8afce4e1fdbe6872ccb780bb3efbcc3e10d1e
Rajesh-Reddy1 <mrrajeshreddy1@gmail.com>
2024-05-15 13:25:48 +0530

    Init

---
+++ components/icons/ArrowUpDownIcon.tsx
@@ -0,0 +1,21 @@
+export function ArrowUpDownIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m21 16-4 4-4-4" />
+      <path d="M17 20V4" />
+      <path d="m3 8 4-4 4 4" />
+      <path d="M7 4v16" />
+    </svg>
+  );
+}
