8893b94c83bd66fca353d110d40d546880c37b7c
Rajesh-Reddy1 <mrrajeshreddy1@gmail.com>
2024-05-21 14:31:19 +0530

    a

--- components/AuthContext.tsx
+++ components/AuthContext.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 // import { createContext, useContext, useState, useEffect } from "react";
 // import { getAuth, onAuthStateChanged } from "firebase/auth";
 // import { app } from "@/firebaseConfig";
