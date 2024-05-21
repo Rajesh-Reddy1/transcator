639879305d11e1a5b15e630532909e2b5c0ab35b
Rajesh-Reddy1 <mrrajeshreddy1@gmail.com>
2024-05-21 14:18:30 +0530

    qwe

--- components/AuthContext.tsx
+++ components/AuthContext.tsx
@@ -27,7 +27,7 @@
 //     </AuthContext.Provider>
 //   );
 // }
-'use client'
+// 'use client'
 import { createContext, useContext, useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "@/firebaseConfig";
